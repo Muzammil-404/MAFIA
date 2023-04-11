4bb6e358c9bd5627133383deff80d945df99b3ff
Muhammad Muzammil <122248440+Muzammil-404@users.noreply.github.com>
2023-04-11 11:38:02 +0500

    Update MAFIA.py

--- MAFIA.py
+++ MAFIA.py
@@ -1,9 +1,9 @@
 import os,platform
-#os.system('git pull')
+os.system('git pull')
  
 MAFIA=platform.architecture()[0]
 if MAFIA=="32bit":
     print('Sorry 32 Bit Not Supported...')
 elif MAFIA=="64bit":
-    print('Command is in update wait we will fix it soon !')
-    #__import__("MAFIA")
+    #print('Command is in update wait we will fix it soon !')
+    __import__("MAFIA")
