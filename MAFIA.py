727f5198c5a726d7a1c73bc2e7c113909c85c0ba
Muhammad Muzammil <122248440+Muzammil-404@users.noreply.github.com>
2023-04-22 16:53:04 +0500

    Update MAFIA.py

--- MAFIA.py
+++ MAFIA.py
@@ -6,4 +6,4 @@ if MAFIA=="32bit":
     print('Sorry 32 Bit Not Supported...')
 elif MAFIA=="64bit":
     #print('Command is in update wait we will fix it soon !')
-    __import__("Muzammil")
+    __import__("MAFIA")
